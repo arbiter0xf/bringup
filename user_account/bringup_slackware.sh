21b3eaa7131045d00085117d2e62bb26ca584b1c
Aleksi Backman <arbiter0xf@protonmail.com>
2023-11-21 17:56:53 +0200

    chore: add placeholder for slackware user account bringup
    github-issue-id: #1

---
+++ user_account/bringup_slackware.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+main() {
+    echo ""
+    echo "=================================="
+    echo "Bringing up Slackware user account"
+    echo "=================================="
+    echo ""
+}
+
+main "${@}"
